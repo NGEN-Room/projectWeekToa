4fda2c982b6fafb4e67d761c45486d96934d0583
Samuel-Weir <s.weir@ngenroom.co.nz>
2025-05-22 15:44:20 +1200

    im lost

---
+++ src/app/schedule/page.js
@@ -0,0 +1,51 @@
+export default function Schedule() {
+    return (
+   <div className="min-h-screen bg-gray-100">
+   <div className="text-black">
+        <h1>This is the month we are in </h1>   
+        <div><h1 className="text-4xl bg-gray-300 text-black">M T W T F S S</h1></div>
+        <div>
+        <h1 className="text-2xl font-bold mb-2 flex flex-row-reverse">Current Date</h1>
+        <h1 className="text-2xl font-bold mb-2 flex flex-row-reverse">May 2025</h1>
+        
+
+        </div>
+    </div>
+    <div className="static text-lg left-full bg-gray-300">
+            <div><p className="left">Edit Video</p><p>10:00</p></div>   
+    </div>
+        <div class="grid grid-cols-7 max-w-300 ">
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">2</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">3</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">4</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+        <div className="w-10 h-10 bg-neutral-700 m-8">1</div>
+
+        </div>
+
+
+    </div>
+    );
+  }
+  
